b715fc58b227f53f55827423f13b1376d8f28d04
Jiyun Park <jiyun@yolocorp.kr>
2022-12-09 18:53:33 +0900

    test

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-# slackAutoMessage
-pip install requests schedule load_dotenv
-python3 latetime.py
+# slackAutoMessage   
+## install packages   
+pip install requests python-dotenv   
+   
+## test   
+python3 test.py   
+   
+## env   
+vim .env   
+   
+## cron   
+crontab -e   
+0 3 * * 1-5 /usr/bin/python3 /home/ubuntu/slackAutoMessage/latetime.py   
+   
+## log   
+0 3 * * 1-5 /usr/bin/python3 /home/ubuntu/slackAutoMessage/latetime.py > /home/ubuntu/slackAutoMessage/test.log 2>&1
--- test.py
+++ test.py
@@ -1,15 +1,17 @@
 import requests
 import os
+import time
 from dotenv import load_dotenv
 load_dotenv()
 TOKEN_BOT = os.getenv("TOKEN_BOT")
+LOCATION = os.getenv("LOCATION")
 
 def post_message(token, channel, text):
   response = requests.post("https://slack.com/api/chat.postMessage",
     headers={"Authorization": "Bearer "+token},
     data={"channel": channel,"text": text}
   )
-  print(response)
+  print(LOCATION + ": ", response, time.localtime())
 
 def 봇테스트():
   post_message(TOKEN_BOT, "test-jiyun", "하이")
