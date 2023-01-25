6124f1804bcc3cca8111267fe611b21b12b0b870
Jiyun Park <jiyun@yolocorp.kr>
2023-01-25 18:39:29 +0900

    feat : 구글캘린더

---
+++ googlecal/quickstart.py
@@ -0,0 +1,68 @@
+from __future__ import print_function
+
+import datetime
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
+
+def main():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('calendar', 'v3', credentials=creds)
+
+        # Call the Calendar API
+        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+        today = datetime.date.today().isoformat() + 'T00:00:00.000000Z'
+        todayEnd = datetime.date.today().isoformat() + 'T15:00:00.000000Z'
+        tomorrow = (datetime.date.today() + datetime.timedelta(days=1)).isoformat() + 'T00:00:00.000000Z'
+        print('Getting the upcoming 10 events', now, today, todayEnd)
+        # calendarId='primary'
+        events_result = service.events().list(calendarId='c_6930030a87cd6d5bed523c900b00ef8396a98ca302aa7f6b67652b65a5ba70e5@group.calendar.google.com', 
+                                              timeMin=today, timeMax=todayEnd, 
+                                              maxResults=10, singleEvents=True,
+                                              orderBy='startTime').execute()
+        events = events_result.get('items', [])
+
+        if not events:
+            print('No upcoming events found.')
+            return
+
+        # Prints the start and name of the next 10 events
+        for event in events:
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            print(start, event['summary'])
+
+    except HttpError as error:
+        print('An error occurred: %s' % error)
+
+
+if __name__ == '__main__':
+    main()
